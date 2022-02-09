2d8d468887e81468d2c4cb01b52c2b6ddc1096cb
Hornung Frank (XC-AD/EPI2) <frank.hornung@de.bosch.com>
2022-02-09 08:48:12 +0100

    add error handling for jdbc exceptions in note-creation

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
@@ -4,10 +4,13 @@ import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
 
 @Controller
 public class NoteController {
@@ -23,25 +26,34 @@ public class NoteController {
     @PostMapping("/notesubmit")
     public String submitNote(@ModelAttribute("noteObject") Note note, Authentication authentication, Model model) {
 
-        if (note.getNoteId() != null){
-            System.out.println("noteId is: " + note.getNoteId().toString());
-            if(note.getUserId() == null){
-                User currentUser = userService.getUser(authentication.getName());
-                note.setUserId(currentUser.getUserId());
-            }
-
-            noteService.updateNote(note);
-        }
-        else{
-            System.out.println("noteId is NULL");
-
+        // add the userId to the note pojo in case missing
+        if(note.getUserId() == null){
             User currentUser = userService.getUser(authentication.getName());
             note.setUserId(currentUser.getUserId());
-
-            noteService.createNote(note);
         }
 
-        model.addAttribute("success", "Success");
+        // update existing note
+        if (note.getNoteId() != null){
+            System.out.println("noteId is: " + note.getNoteId().toString());
+            try{
+                noteService.updateNote(note);
+                model.addAttribute("success", "Success");
+            }catch(DataIntegrityViolationException e){
+                model.addAttribute("failed", "saving your note had issues. Your note is unchanged! Hint: the description you put may not exceed 1000 characters!");
+                e.printStackTrace();
+            }
+        }
+        // new note, needs creation
+        else{
+            System.out.println("noteId is NULL");
+            try{
+                noteService.createNote(note);
+                model.addAttribute("success", "Success");
+            }catch(DataIntegrityViolationException e){
+                model.addAttribute("failed", "saving your note had issues. Hint: the description you put may not exceed 1000 characters!");
+                e.printStackTrace();
+            }
+        }
         return "result";
     }
 
